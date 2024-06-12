ca71b0ad1f86662a0592a23a53a492151c4f6fed
motasm <ps171179@summacollege.nl>
2024-06-12 09:24:00 +0200

    Beast/

---
+++ Beast/Beast/Classes/DbHandler.cs
@@ -0,0 +1,150 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows;
+
+namespace Beast.Classes
+{
+    class DbHandler
+    {
+        //Hier maken we een MySqlConnection met o.a. Server;Database;Uid;Pwd;
+        MySqlConnection _connection = new MySqlConnection("Server=localhost;Database=beast2;Uid=root;Pwd=;");
+        public DataTable beastnummer(string beastnummer)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                _connection.Open();
+                MySqlCommand myCommand = _connection.CreateCommand();
+                
+                myCommand.CommandText = $"SELECT * FROM `demo` WHERE `beastId` = '{beastnummer}'";
+                
+                
+                
+                MySqlDataReader reader = myCommand.ExecuteReader();
+                
+                dt.Load(reader);
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("your kkr database is broken fix it!!!\n" + e.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return dt;
+        }
+
+        public DataTable GetNumber()
+        {
+            //Maak een DataTable
+            DataTable mytable = new DataTable();
+            try
+            {
+                //Open de verbinding
+                _connection.Open();
+                //Maak een Commando via de connection
+                MySqlCommand myCommand = _connection.CreateCommand();
+                //Zet de CommandText
+                myCommand.CommandText = "SELECT * FROM `demo`";
+                //Voer de MySqlDataReader uit
+                MySqlDataReader reader = myCommand.ExecuteReader();
+                //Laad het resultaat in de DataTable
+                mytable.Load(reader);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("your kkr database is broken fix it!!!\n" + e.Message);
+            }
+            finally
+            {
+                //Sluit de verbinding
+                _connection.Close();
+            }
+
+
+            //Return de DataTable
+            return mytable;
+        }
+
+        public int UpdateNumber(string waarde, string rijnummer, string kleur)
+        {
+            int amountChanged = 0;
+            try
+            {
+                // Open de verbinding
+                _connection.Open();
+
+                // Maak een Commando via de connection
+                MySqlCommand myCommand = _connection.CreateCommand();
+
+                // Zet de CommandText met de nieuwe tekst en voeg een WHERE-clausule toe
+
+                myCommand.CommandText = $"UPDATE `demo` SET `nummer`={waarde}, `kleur`='{kleur}' WHERE `beastId`= {rijnummer}";
+
+
+                // ExecuteNonQuery en zet het resultaat in de int
+                amountChanged = myCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Fout opgetreden bij het bijwerken van de database: " + e.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return amountChanged;
+        }
+
+        public int toevoegen(string waarde, string kleur)
+        {
+            int amountChanged = 0;
+            try
+            {
+                _connection.Open();
+                MySqlCommand myCommand = _connection.CreateCommand();
+
+                myCommand.CommandText = $"INSERT INTO `demo`(`nummer`, `kleur`) VALUES ('{waarde}','{kleur}')";
+
+                amountChanged = myCommand.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Fout opgetreden bij het bijwerken van de database: " + e.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return amountChanged;
+        }
+        public int Verwijderen(string rijnummer)
+        {
+            int amountChanged = 0;
+            try
+            {
+                _connection.Open();
+                MySqlCommand myCommand = _connection.CreateCommand();
+
+                myCommand.CommandText = $"DELETE FROM `demo` WHERE `beastId`= {rijnummer}";
+
+                amountChanged = myCommand.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Fout opgetreden bij het bijwerken van de database: " + e.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return amountChanged;
+        }
+    }
+}
---
+++ Beast/Beast/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using Beast.Classes;
+using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Beast
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DbHandler db = new DbHandler();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Beast_nummer_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                string beastnummer = tbRijnummer.Text;
+                DataTable result = db.beastnummer(beastnummer);
+
+
+                string kleur = result.Rows[0]["kleur"].ToString().ToLower();
+
+                switch (kleur)
+                {
+                    case "red":
+                        tbNumber.Background = Brushes.Red;
+                        break;
+                    case "blue":
+                        tbNumber.Background = Brushes.Blue;
+                        break;
+                    case "yellow":
+                        tbNumber.Background = Brushes.Yellow;
+                        break;
+                    case "black":
+                        tbNumber.Background = Brushes.Black;
+                        break;
+                    case "green":
+                        tbNumber.Background = Brushes.Green;
+                        break;
+                    default:
+                        MessageBox.Show("Ongeldige kleur.");
+                        return;
+                }
+
+                tbNumber.Text = result.Rows[0]["nummer"].ToString();
+
+
+
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Er is een fout opgetreden bij het bijwerken van de gegevens: " + ex.Message);
+            }
+
+        }
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataTable result = db.GetNumber();
+                dataGrid.ItemsSource = result.DefaultView;
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Er is een fout opgetreden bij het bijwerken van de gegevens: " + ex.Message);
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            string rijnummer = tbRijnummer.Text;
+            string waarde = tbValue.Text;
+            string kleur = tbKleur.SelectedItem != null ? ((ComboBoxItem)tbKleur.SelectedItem).Content.ToString() : "";
+
+            if (string.IsNullOrEmpty(rijnummer) || string.IsNullOrEmpty(waarde) || string.IsNullOrEmpty(kleur))
+            {
+                MessageBox.Show("Vul alle velden in voordat u kunt bijwerken.");
+                return; // Stop de methode als een van de velden leeg is
+            }
+
+            int rowsAffected = db.UpdateNumber(waarde, rijnummer, kleur);
+            if (rowsAffected < 0)
+
+            {
+                MessageBox.Show("Fout opgetreden bij het bijwerken van de database");
+            }
+            btnGo_Click(null, null);
+            Beast_nummer_Click(null, null);
+        }
+
+        private void Toevoegen_Click(object sender, RoutedEventArgs e)
+        {
+            string waarde = tbValue.Text;
+            string kleur = tbKleur.SelectedItem != null ? ((ComboBoxItem)tbKleur.SelectedItem).Content.ToString() : "";
+
+            if (string.IsNullOrEmpty(waarde) || string.IsNullOrEmpty(kleur))
+            {
+                MessageBox.Show("Vul alle velden in voordat u kunt bijwerken.");
+                return; // Stop de methode als een van de velden leeg is
+            }
+
+            int rowsAffected = db.toevoegen(waarde, kleur);
+            if (rowsAffected < 0)
+
+            {
+                MessageBox.Show("Fout opgetreden bij het bijwerken van de database");
+            }
+
+            btnGo_Click(null, null);
+        }
+
+        private void Vrwijderen_Click(object sender, RoutedEventArgs e)
+        {
+            string rijnummer = tbRijnummer.Text;
+
+            if (string.IsNullOrEmpty(rijnummer))
+            {
+                MessageBox.Show("Vul de rijnummer die u wilt verijderen");
+                return; // Stop de methode als een van de velden leeg is
+            }
+
+            int rowsAffected = db.Verwijderen(rijnummer);
+            if (rowsAffected < 0)
+
+            {
+                MessageBox.Show("Fout opgetreden bij het bijwerken van de database");
+            }
+            btnGo_Click(null, null);
+        }
+    }
+}
